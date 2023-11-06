58d7712a49beabc5789bda6318d14805ca397eae
Felicia Udosen <udosenfelicia@gmail.com>
2023-11-06 10:52:14 +0000

    finished the UI for the dashboard

---
+++ src/app/data/data.js
@@ -0,0 +1,113 @@
+export const data = [
+  {
+    id: 1,
+    name: {
+      first: "John",
+      last: "Smith",
+    },
+    total: 2795.95,
+    status: "On Hold",
+    method: "Paypal",
+    date: "15 mins ago",
+  },
+  {
+    id: 2,
+    name: {
+      first: "Chris",
+      last: "Adam",
+    },
+    total: 1195.95,
+    status: "Processing",
+    method: "Paypal",
+    date: "23 Minutes ago",
+  },
+  {
+    id: 3,
+    name: {
+      first: "Sarah",
+      last: "Smith",
+    },
+    total: 495.85,
+    status: "Completed",
+    method: "Visa",
+    date: "1 Hourago",
+  },
+  {
+    id: 4,
+    name: {
+      first: "Joseph",
+      last: "Choo",
+    },
+    total: 150.45,
+    status: "Processing",
+    method: "Mastercard",
+    date: "1 Hour ago",
+  },
+  {
+    id: 5,
+    name: {
+      first: "Steve",
+      last: "Harding",
+    },
+    total: 175.25,
+    status: "On Hold",
+    method: "Mastercard",
+    date: "2 Hour ago",
+  },
+  {
+    id: 6,
+    name: {
+      first: "Laura",
+      last: "Croft",
+    },
+    total: 1295.75,
+    status: "Completed",
+    method: "Check",
+    date: "3 Hour ago",
+  },
+  {
+    id: 7,
+    name: {
+      first: "Michael",
+      last: "Jones",
+    },
+    total: 89.95,
+    status: "Completed",
+    method: "MasterCard",
+    date: "3 Hour ago",
+  },
+  {
+    id: 8,
+    name: {
+      first: "James",
+      last: "Bond",
+    },
+    total: 689.45,
+    status: "Completed",
+    method: "Visa",
+    date: "7 Hour ago",
+  },
+  {
+    id: 9,
+    name: {
+      first: "Haley",
+      last: "Whiting",
+    },
+    total: 14.99,
+    status: "Completed",
+    method: "Paypal",
+    date: "1 day ago",
+  },
+
+  {
+    id: 10,
+    name: {
+      first: "Tin",
+      last: "Thomas",
+    },
+    total: 218.99,
+    status: "Completed",
+    method: "MasterCard",
+    date: "1 day ago",
+  },
+];
